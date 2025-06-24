9e3fc2b4460415361b50b397a70411239a0e15f1
mengde <huai.mengde@gmail.com>
2025-06-24 18:14:55 +0200

    upgraded secretword game

--- secretword.py
+++ secretword.py
@@ -1,20 +1,36 @@
 import random
 
-secret_words = ["girlfriend", "boyfriend", "friend"]
+secret_words = ["girlfriend", "boyfriend", "friend", "apple", "ghost"]
 secret_word = random.choice(secret_words)
-guess = ""
+guesses = ""
 guess_count = 0
-guess_limit = 3
+guess_limit = 12
 out_of_guesses = False
 
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count += 1
-    else:
-        out_of_guesses = True
+print("Guess the character: ")
 
-if out_of_guesses:
-    print("Out of guesses. You lose")
-else:
-    print("You win!")
+while guess_limit > 0:
+    failed = 0
+
+    for char in secret_word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_")
+            failed += 1
+    
+    if failed == 0:
+        print(f"\nYou win The word was {secret_word}")
+        break
+
+    print()
+    guess = input("Guess the character: ")
+
+    guesses += guess
+    if guess not in secret_word:
+        guess_limit -= 1
+        print("Wrong!")
+        print("You have", + guess_limit, "more guesses")
+
+        if guess_limit == 0:
+            print("You lose")
